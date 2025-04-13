93bcda684cc3265e250b750473c4cc469fe5012d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-14 00:55:13 +0430

    git init

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <StatusBar style='dark' />
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+        Welcome to the Threads app!
+      </Text>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
