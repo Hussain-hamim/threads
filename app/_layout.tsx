02f55c76172d593dfce1f69351efc7326ff11498
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-15 16:00:28 +0430

    eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
+import { Text } from 'react-native';
+
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const InitialLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index' />
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
