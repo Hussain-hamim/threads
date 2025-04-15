8001c2f86d91db64dc5878867506f11eb02ce6df
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-15 15:14:45 +0430

    install secure storage

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
         Welcome to the Threads app!
       </Text>
-      <Text>Hello world oy oi</Text>
+      <Text>Hello world oy</Text>
     </View>
   );
 }
