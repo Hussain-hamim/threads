1938234028089b7975a3698ff4c848d3f8024e7c
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-18 08:47:55 +0430

    add http routing for Clerk webhook; implement doSomething action

--- convex/http.ts
+++ convex/http.ts
@@ -1,6 +1,21 @@
+import { httpRouter } from 'convex/server';
 import { httpAction } from './_generated/server';
+
+const http = httpRouter();
 
 export const doSomething = httpAction(async () => {
   // implementation will be here
+  console.log('Doing something... again!');
   return new Response();
 });
+
+http.route({
+  path: '/clerk-users-webhook',
+  method: 'GET',
+  handler: doSomething,
+});
+
+//https://<your deployment name>.convex.site
+// https://perfect-swan-977.convex.site/clerk-users-webhook
+
+export default http;
