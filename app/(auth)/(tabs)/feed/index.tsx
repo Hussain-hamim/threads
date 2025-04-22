6fcd22b574087f218609be065242d71cef1d2d4b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-22 15:31:28 +0430

    refactor: enhance feed component with safe area insets and paginated query for threads

--- app/(auth)/(tabs)/feed/index.tsx
+++ app/(auth)/(tabs)/feed/index.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { usePaginatedQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const feed = () => {
+  const { top } = useSafeAreaInsets();
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.messages.getThreads,
+    {},
+    { initialNumItems: 5 }
+  );
+
   return (
-    <View>
+    <View style={{ marginTop: top }}>
       <Text>feed</Text>
     </View>
   );
--- convex/messages.ts
+++ convex/messages.ts
@@ -1,7 +1,8 @@
 import { v } from 'convex/values';
-import { mutation, query } from './_generated/server';
+import { mutation, query, QueryCtx } from './_generated/server';
 import { getCurrentUserOrThrow } from './users';
 import { paginationOptsValidator } from 'convex/server';
+import { Id } from './_generated/dataModel';
 
 export const addThreadMessage = mutation({
   args: {
@@ -43,12 +44,43 @@ export const getThreads = query({
     } else {
       threads = await ctx.db
         .query('messages')
-        .filter((q) => q.eq(q.field('threadId'), undefined))
+        .filter((q) => q.eq(q.field('threadId'), null))
         .order('desc')
         .paginate(args.paginationOpts);
     }
+
+    const messagesWithCreator = await Promise.all(
+      threads.page.map(async (thread) => {
+        const creator = await getMessageCreator(ctx, thread.userId);
+
+        return {
+          ...thread,
+          creator,
+        };
+      })
+    );
+
+    return {
+      ...threads,
+      page: messagesWithCreator,
+    };
   },
 });
+
+const getMessageCreator = async (ctx: QueryCtx, userId: Id<'users'>) => {
+  const user = await ctx.db.get(userId);
+
+  if (!user?.imageUrl || user.imageUrl.startsWith('http')) {
+    return user;
+  }
+
+  const imageUrl = await ctx.storage.getUrl(user.imageUrl as Id<'_storage'>);
+
+  return {
+    ...user,
+    imageUrl,
+  };
+};
 
 export const generateUploadUrl = mutation({
   handler: async (ctx) => {
